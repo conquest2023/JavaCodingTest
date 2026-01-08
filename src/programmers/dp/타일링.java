4cb3c91c7265695d5b252e3fe27c5e26847d81e1
conquest2023 <dlqns928@naver.com>
2026-01-08 17:52:16 +0900

    Solve

--- src/programmers/dp/타일링.java
+++ src/programmers/dp/타일링.java
@@ -1,2 +1,23 @@
-package programmers.dp;public class 타일링 {
+package programmers.dp;
+
+public class 타일링 {
+
+
+    static int[] dp=new int[60000];
+    public static void main(String[] args) {
+
+        int solution = solution(4);
+        System.out.println(solution);
+    }
+
+
+    public static int solution(int n) {
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        for (int i = 3; i <=n ; i++) {
+            dp[i]=(dp[i-1]+dp[i-2]) % 1000000007;
+        }
+        return dp[n];
+    }
 }
