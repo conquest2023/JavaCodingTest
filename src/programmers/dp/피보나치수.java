273cfe5f99b50c3b96c8f3ef34f0cd377eb57ac4
conquest2023 <dlqns928@naver.com>
2026-01-08 17:52:27 +0900

    Solve

--- src/programmers/dp/피보나치수.java
+++ src/programmers/dp/피보나치수.java
@@ -1,2 +1,21 @@
-package programmers.dp;public class 피보나치수 {
+package programmers.dp;
+
+public class 피보나치수 {
+
+    static int dp[]=new int[100001];
+
+    public static void main(String[] args) {
+        int solution = solution(100000);
+        System.out.println(solution);
+    }
+
+
+    public static int solution(int n) {
+        dp[0]=0;
+        dp[1]=1;
+        for (int i = 2 ; i <= n ; i++) {
+            dp[i]=dp[i-2]+dp[i-1];
+        }
+        return dp[n];
+    }
 }
