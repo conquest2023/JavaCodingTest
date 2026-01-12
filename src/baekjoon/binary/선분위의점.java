ac0f4d49516718cca0fe7d8a7f57e6590de321a6
conquest2023 <dlqns928@naver.com>
2026-01-12 17:13:02 +0900

    Not Solve

--- src/baekjoon/binary/선분위의점.java
+++ src/baekjoon/binary/선분위의점.java
@@ -1,4 +1,4 @@
-package baekjoon.math;
+package baekjoon.binary;
 
 import java.util.Arrays;
 import java.util.Scanner;
@@ -8,22 +8,49 @@ public class 선분위의점 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        sc.nextLine();
-        int[] array = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::valueOf).toArray();
+
+        int[] points = new int[n];
+        for (int i = 0; i < n; i++) points[i] = sc.nextInt();
+
+        // 이분 탐색을 위해 반드시 정렬!
+        Arrays.sort(points);
+
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < m; i++) {
-            String[] split = sc.nextLine().split(" ");
-            int count=0;
-            int start = Integer.parseInt(split[0]);
-            int end = Integer.parseInt(split[1]);
-            for (int j = start; j <= end; j++) {
-                for (int value : array) {
-                    if (value == j) {
-                        count += 1;
-                        break;
-                    }
-                }
-            }
-            System.out.println(count);
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+
+            // start 이상의 값이 처음 나오는 인덱스
+            int leftIdx = getLowerBound(points, start);
+            // end 이하의 값이 마지막으로 나오는 인덱스 (혹은 end 초과 값이 처음 나오는 인덱스)
+            int rightIdx = getUpperBound(points, end);
+
+            sb.append(rightIdx - leftIdx).append("\n");
         }
+        System.out.print(sb);
+    }
+
+    // target 이상의 값이 처음 나오는 위치
+    private static int getLowerBound(int[] arr, int target) {
+        int left = 0, right = arr.length;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] >= target)
+                right = mid;
+            else left = mid + 1;
+        }
+        return left;
+    }
+
+    // target 초과 값이 처음 나오는 위치
+    private static int getUpperBound(int[] arr, int target) {
+        int left = 0, right = arr.length;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] > target)
+                right = mid;
+            else left = mid + 1;
+        }
+        return left;
     }
 }
